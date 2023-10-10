5b0ef473c37196d13f611289d4060be0e2549cfe
faja <mcabaj@gmail.com>
2023-10-10 23:50:52 +0200

    bpf

--- ebpf/01_book-learning-ebpf/provision.sh
+++ ebpf/01_book-learning-ebpf/provision.sh
@@ -3,4 +3,12 @@
 echo hello from provisioner
 
 apt-get update -y
+
+# BCC | ch02
 apt-get install -y bpfcc-tools libbpfcc libbpfcc-dev linux-headers-$(uname -r)
+
+# bpftool | ch03
+apt-get install -y bpftool
+
+# make and clang | ch03
+apt-get install -y make clang
